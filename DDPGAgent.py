82a730363146f41e79151a4d60a856c2a3ca9e14
jmkGithub <44000775+jmkGithub@users.noreply.github.com>
2018-11-13 20:33:39 -0600

    Update DDPGAgent.py

--- DDPGAgent.py
+++ DDPGAgent.py
@@ -14,8 +14,8 @@ BUFFER_SIZE = int(2e5)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1.5e-4         # learning rate of the actor 
-LR_CRITIC = 3e-4        # learning rate of the critic
+LR_ACTOR = 3e-5         # learning rate of the actor 
+LR_CRITIC = 1.5e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0000   # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
